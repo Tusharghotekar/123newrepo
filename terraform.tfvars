c4050548da2af1dcf1141a4af4dab0298c014008
tushar <tusharghotekar92@gmail.com>
2024-02-02 21:12:21 +0530

    azure infra

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+location             = "eastus"
+resource_group_name  = "tuharsnewrg00"
+storage_account_name = "newstorge151947"
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "location of resource group"
+}
+variable "resource_group_name" {
+  description = "name of rg"
+
+}
+variable "storage_account_name" {
+  description = "storage account name"
+
+}
